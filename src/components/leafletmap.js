7bcf5a85d85f24eb5354efb5a27e291ea82f8a70
duksieg <duksieg41826@hotmail.com>
2021-09-30 22:50:51 +0700

    update map

--- src/components/leafletmap.js
+++ src/components/leafletmap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {  Popup, TileLayer, Marker, MapContainer } from "react-leaflet";
-import L from 'leaflet/dist/leaflet'
+import L from 'leaflet/dist/leaflet-src.esm'
 import 'leaflet/dist/leaflet.css'
 import * as mapicon from '../components/mapicon'
 class LeafletMap extends React.Component {
